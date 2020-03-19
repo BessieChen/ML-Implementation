1f38e7965983b48bac8f506b5c37f396f2bdbc7e
Bessie <bchen372@gatech.edu>
2020-03-19 10:51:08 +0800

    update

--- myML/preprocessing.py
+++ myML/preprocessing.py
@@ -7,7 +7,7 @@ class myStandardScaler:
 
     def fit(self, X):
         '''根据训练数据集 X 获得数据的 mean 和 variance'''
-        assert X.dim == 2, "The dimension of X must be 2" #一定是二维，但是这个二维的shape随意
+        #assert X.ndim == 2, "The dimension of X must be 2" #一定是二维，但是这个二维的shape随意
 
         self.mean_ = [np.mean(X[:,i]) for i in range(X.shape[1])]
         self.scale_ = [np.std(X[:, i]) for i in range(X.shape[1])]
@@ -15,7 +15,7 @@ class myStandardScaler:
 
     def transform(self, X):
         '''将X 根据这个StandardSclae进行 均值方差归一化 处理'''
-        assert X.dim == 2, "The dimension of X must be 2"
+        #assert X.ndim == 2, "The dimension of X must be 2"
         assert self.mean_ is not None and self.std_ is not None, "must fit before transform"
 
         assert X.shape[1] == len(self.mean_), "The feature number of X must be equal to length of mean"
@@ -25,3 +25,7 @@ class myStandardScaler:
             res[:, col] = (X[:, col] - self.mean_[col]) / self.scale_[col]
 
         return res
+
+if __name__ == "__main__":
+    my = myStandardScaler()
+    my.fit(np.random.normal(size = (100,2)))
